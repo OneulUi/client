edd5303cc785314dd07c5df95f43cc6ce571586d
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2024-04-25 02:40:11 +0900

    Feat: 사용자 location 검색 후 주소저장

--- app/location/page.tsx
+++ app/location/page.tsx
@@ -1,6 +1,28 @@
+"use client";
 import Button from "@/components/button/Button";
 import DaumPostcode from "react-daum-postcode";
+import { Address } from "react-daum-postcode";
+import { useState, useEffect } from "react";
 export default function Location() {
+  const [showDaumPostcode, setShowDaumPostcode] = useState<boolean>(false);
+  const [address, setAddress] = useState<string>("");
+  const handleInputFocus = () => {
+    setShowDaumPostcode(true);
+  };
+  const themeObj = {
+    postcodeTextColor: "#FA7142",
+    emphTextColor: "#333333",
+  };
+
+  const completeHandler = (data: Address) => {
+    console.log(data);
+    setAddress(data.address);
+  };
+
+  const reloadHandler = () => {
+    window.location.reload();
+  };
+
   return (
     <main className="flex flex-col w-full p-6 m-6">
       <section className="flex flex-col mb-4">
@@ -11,11 +33,25 @@ export default function Location() {
           해당 정보는 추후 추천 정보에 반영됩니다.
         </div>
       </section>
-      <section className="flex flex-col justify-center items-center mt-4">
+      <section className="flex flex-col mt-4">
         <input
           className="border-2 p-4 w-full rounded-2xl"
           placeholder="사는 지역 검색하기"
+          onFocus={handleInputFocus}
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
         />
+        {showDaumPostcode && (
+          <DaumPostcode onComplete={completeHandler} theme={themeObj} />
+        )}
+        {address === "" && (
+          <button
+            className="text-right p-2 mt-2 font-extralight"
+            onClick={reloadHandler}
+          >
+            주소 다시 검색하기
+          </button>
+        )}
         <Button text="오늘의 시작하기" href="/location" />
       </section>
     </main>
