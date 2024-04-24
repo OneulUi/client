c686eb98d2ad6d2527ea83cd6d6a61f7159c7824
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2024-04-24 19:43:53 +0900

    [Design]렌더 페이지 반응형 작업 완료

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,14 @@ import sun from "../assets/sun.png";
 export default function Home() {
   return (
     <main className="">
-      <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center overflow-hidden">
         <Image
           src="https://cdn.pixabay.com/photo/2024/02/28/14/01/woman-8602128_1280.png"
-          layout="Fill"
+          className="w-[100vw] h-[100vh]"
           width={500}
           height={500}
           alt="girl"
           priority
-          sizes="(max-width: 500px) 100vw, 500px"
         />
         <div className="absolute inset-x-0 bottom-20 flex justify-center">
           <Button text="시작하기" href="/signin" />
