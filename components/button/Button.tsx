9a6408f3f8525df3a199773022773f74d3f77384
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2024-04-24 19:45:58 +0900

    Design: 공용 button transition 적용

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Button({ text, href }: ButtonText) {
   return (
     <Link href={href}>
-      <button className="bottom-40 bg-gray-900 text-white w-[45vh] h-[8vh] flex items-center justify-center rounded-3xl">
+      <button className="bottom-40 bg-gray-900 text-white w-[45vh] h-[8vh] flex items-center justify-center rounded-3xl transition-transform hover:scale-95">
         {text}
       </button>
     </Link>
