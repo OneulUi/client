4ab65878dc0f007163789705bfaec60c0bcd8025
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2024-04-24 20:51:11 +0900

    Design: 렌딩 페이지 블러 처리

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,14 @@ export default function Home() {
       <div className="flex flex-col items-center justify-center overflow-hidden">
         <Image
           src="https://cdn.pixabay.com/photo/2024/02/28/14/01/woman-8602128_1280.png"
-          className="w-[100vw] h-[100vh]"
+          className="w-[100vw] h-[100vh] "
           width={500}
           height={500}
           alt="girl"
           priority
         />
+
+        <div className="absolute bottom-0 w-[500Px] 3xl:w-full h-1/3 bg-gradient-to-t from-gray-600"></div>
         <div className="absolute inset-x-0 bottom-20 flex justify-center">
           <Button text="시작하기" href="/signin" />
         </div>
