785d824f57239f2ecb8c22ffc69f4073f4913f42
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2024-04-28 03:16:37 +0900

    Feat:ootd page component, header 구현

--- app/ootd/page.tsx
+++ app/ootd/page.tsx
@@ -1,3 +1,18 @@
+import Header from "@/components/ootd/Header";
+import Weather from "@/components/ootd/Weather";
+import { GoPencil } from "react-icons/go";
+import { IoIosSearch } from "react-icons/io";
 export default function Ootd() {
-  return <div>ootd</div>;
+  return (
+    <main className="w-full flex flex-col items-center">
+      <section className="flex items-center justify-between p-6 w-full backdrop-blur-[1px]">
+        <span className="text-gray-700 text-[20px] font-bold">날씨 OOTD</span>
+        <span className="flex">
+          <GoPencil className="mr-4" size={22} />
+          <IoIosSearch size={24} />
+        </span>
+      </section>
+      <Weather />
+    </main>
+  );
 }
--- components/ootd/Text.tsx
+++ components/ootd/Text.tsx
@@ -1,7 +1,10 @@
 export default function Text() {
   return (
-    <section className="flex border-2 rounded-2xl p-4 w-5/6 m-4 h-[300px] bg-gray-200">
-      <div className="font-thin">글 작성하기</div>
-    </section>
+    <>
+      <section className="flex border-2 rounded-2xl p-4 w-5/6 m-4 h-[300px] bg-gray-200">
+        <div className="font-thin">글 작성하기</div>
+      </section>
+      <button className="m-4 border-2 p-2 rounded-xl">올리기</button>
+    </>
   );
 }
