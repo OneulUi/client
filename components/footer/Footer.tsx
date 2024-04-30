a90a762d4442261b8ee3192853b7943c00c369e9
wooka000 <wooka000@naver.com>
2024-04-30 23:46:20 +0900

    Feat : path에 따른 Footer component Tab bar 구현

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,25 +1,46 @@
+"use client";
+
 import Link from "@/node_modules/next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
+import image from "../../assets/sun.png";
 
 export default function Footer() {
+  const pathName = usePathname();
   return (
     <footer className="fixed bottom-0 border-t-2 py-4 w-[500px] h-[10vh]">
       <div className="flex w-full items-center justify-around">
         <Link href="/main" className="flex flex-col items-center">
-          <Image width="30" height="30" src="" alt="weather" />
-          오늘 날씨
+          <Image width="30" height="30" src={image} alt="weather" />
+          {pathName === "/main" ? (
+            <div className="text-blue-300">오늘 날씨</div>
+          ) : (
+            <div>오늘 날씨</div>
+          )}
         </Link>
         <Link href="/recommend" className="flex flex-col items-center">
-          <Image width="30" height="30" src="" alt="recommend" />
-          날씨 옷 추천
+          <Image width="30" height="30" src={image} alt="recommend" />
+          {pathName === "/recommend" ? (
+            <div className="text-blue-300">날씨 옷 추천</div>
+          ) : (
+            <div>날씨 옷 추천</div>
+          )}
         </Link>
         <Link href="ootd" className="flex flex-col items-center">
-          <Image width="30" height="30" src="" alt="ootd" />
-          날씨 OOTD
+          <Image width="30" height="30" src={image} alt="ootd" />
+          {pathName === "/ootd" ? (
+            <div className="text-blue-300">날씨 OOTD</div>
+          ) : (
+            <div>날씨 OOTD</div>
+          )}
         </Link>
         <Link href="mypage" className="flex flex-col items-center">
-          <Image width="30" height="30" src="" alt="my" />
-          마이페이지
+          <Image width="30" height="30" src={image} alt="my" />
+          {pathName === "/mypage" ? (
+            <div className="text-blue-300">마이페이지</div>
+          ) : (
+            <div>마이페이지</div>
+          )}
         </Link>
       </div>
     </footer>
