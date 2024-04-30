85dd4735f63bbefd45e48d998d70e8a4cce1571b
wooka000 <wooka000@naver.com>
2024-04-30 17:03:33 +0900

    Feat : Footer Component 구현

---
+++ app/main/page.tsx
@@ -0,0 +1,5 @@
+import Footer from "@/components/footer/Footer";
+
+export default function Home() {
+  return <div>main</div>;
+}
--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,7 +1,46 @@
+import Link from "@/node_modules/next/link";
+
 export default function Footer() {
   return (
-    <footer className="fixed bottom-0">
-      <p>© 2024 Footer</p>
+    <footer className="fixed bottom-0 border-t-2 py-4 w-[500px]">
+      <div className="flex w-full items-center justify-around">
+        <Link href="/main" className="flex flex-col items-center">
+          <img
+            width="30"
+            height="30"
+            src="https://img.icons8.com/ios/50/rainy-weather.png"
+            alt="rainy-weather"
+          />
+          오늘 날씨
+        </Link>
+        <Link href="/recommend" className="flex flex-col items-center">
+          <img
+            width="30"
+            height="30"
+            src="https://img.icons8.com/ios/50/hanger.png"
+            alt="hanger"
+          />
+          날씨 옷 추천
+        </Link>
+        <Link href="ootd" className="flex flex-col items-center">
+          <img
+            width="30"
+            height="30"
+            src="https://img.icons8.com/ios/50/clothes.png"
+            alt="clothes"
+          />
+          날씨 OOTD
+        </Link>
+        <Link href="mypage" className="flex flex-col items-center">
+          <img
+            width="30"
+            height="30"
+            src="https://img.icons8.com/pastel-glyph/64/person-male--v1.png"
+            alt="person-male--v1"
+          />
+          마이페이지
+        </Link>
+      </div>
     </footer>
   );
 }
