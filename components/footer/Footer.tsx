505c01edf719012a06516aa5605440f8023b2361
wooka000 <wooka000@naver.com>
2024-04-30 23:16:05 +0900

    Fix : 외부 이미지 접근 불가에 따른 임시 이미지 제거

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,43 +1,24 @@
 import Link from "@/node_modules/next/link";
+import Image from "next/image";
 
 export default function Footer() {
   return (
     <footer className="fixed bottom-0 border-t-2 py-4 w-[500px] h-[10vh]">
       <div className="flex w-full items-center justify-around">
         <Link href="/main" className="flex flex-col items-center">
-          <img
-            width="30"
-            height="30"
-            src="https://img.icons8.com/ios/50/rainy-weather.png"
-            alt="rainy-weather"
-          />
+          <Image width="30" height="30" src="" alt="weather" />
           오늘 날씨
         </Link>
         <Link href="/recommend" className="flex flex-col items-center">
-          <img
-            width="30"
-            height="30"
-            src="https://img.icons8.com/ios/50/hanger.png"
-            alt="hanger"
-          />
+          <Image width="30" height="30" src="" alt="recommend" />
           날씨 옷 추천
         </Link>
         <Link href="ootd" className="flex flex-col items-center">
-          <img
-            width="30"
-            height="30"
-            src="https://img.icons8.com/ios/50/clothes.png"
-            alt="clothes"
-          />
+          <Image width="30" height="30" src="" alt="ootd" />
           날씨 OOTD
         </Link>
         <Link href="mypage" className="flex flex-col items-center">
-          <img
-            width="30"
-            height="30"
-            src="https://img.icons8.com/pastel-glyph/64/person-male--v1.png"
-            alt="person-male--v1"
-          />
+          <Image width="30" height="30" src="" alt="my" />
           마이페이지
         </Link>
       </div>
