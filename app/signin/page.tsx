3b73edeb40acaccbf5883839058629548d1c5856
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2024-05-02 20:09:11 +0900

    Bug: 프록시 초기설정

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,7 +1,33 @@
+"use client";
 import Image from "next/image";
-import Button from "@/components/button/Button";
-
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { Axios } from "@/api/axios";
+import axios from "axios";
 export default function SignIn() {
+  const router = useRouter();
+
+  const handleButtonClick = async () => {
+    const response = await axios.get(
+      "http://ec2-3-36-88-73.ap-northeast-2.compute.amazonaws.com:8080/oauth2/authorization/google"
+    );
+    console.log(response);
+  };
+
+  const handleRedirect = () => {
+    const url = new URL(window.location.href);
+    const accessToken = url.searchParams.get("access_token");
+    console.log(url);
+    if (accessToken) {
+      localStorage.setItem("ACCESS_TOKEN", accessToken);
+      router.push("/");
+    }
+  };
+
+  useEffect(() => {
+    handleRedirect();
+  }, []);
+
   return (
     <main className="">
       <div className="flex flex-col items-center justify-center overflow-hidden">
@@ -15,11 +41,14 @@ export default function SignIn() {
         />
 
         <div className="absolute bottom-0 w-[500Px] 3xl:w-full h-1/3 bg-gradient-to-t from-gray-600"></div>
-
-        <Button
-          text="시작하기"
-          href="http://3.36.88.73:8080/oauth2/authorization/google"
-        />
+        <div className="absolute inset-x-0 bottom-20 flex justify-center">
+          <button
+            onClick={handleButtonClick}
+            className="bottom-40 bg-gray-900 text-white w-[45vh] h-[7vh] flex items-center justify-center rounded-3xl transition-transform hover:scale-95"
+          >
+            구글 로그인
+          </button>
+        </div>
       </div>
     </main>
   );
