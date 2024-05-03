532f3d46d9135cdbcd124f81f2347ba577195649
wooka000 <wooka000@naver.com>
2024-05-03 23:55:44 +0900

    Design : 메인 페이지 디자인 수정

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,11 +1,11 @@
-//main.page
 import TodayWeather from "@/components/main/TodayWeather";
 import WeatherByHour from "@/components/main/WeatherByHour";
 import Link from "next/link";
-import { IoMdSettings } from "react-icons/io";
+import { IoSettingsOutline } from "react-icons/io5";
 
 export default function Main() {
   return (
+    // 배경 색 온도에 맞게 구현 필요
     <div className="w-full h-[90vh] bg-gradient-to-tr from-blue-500 to-yellow-200 pt-8 px-10 flex flex-col justify-between">
       <div>
         <div className="flex justify-between">
@@ -14,7 +14,7 @@ export default function Main() {
             href=""
             className="bg-white flex justify-between items-center rounded-full p-1 mb-3"
           >
-            <IoMdSettings className="w-6 h-6" />
+            <IoSettingsOutline className="w-6 h-6" />
           </Link>
         </div>
         <TodayWeather />
--- components/main/TodayWeather.tsx
+++ components/main/TodayWeather.tsx
@@ -3,7 +3,20 @@ import { FaArrowDown, FaArrowUp } from "react-icons/fa";
 export default function TodayWeather() {
   return (
     <div className="flex flex-col">
-      <div className="mb-2">2024.04.25</div>
+      <div className="flex items-center mb-2">
+        {/* 드롭다운 css 추가 필요 + api에서 해당 일자에 맞는 날씨 정보 가져오기*/}
+        <form action="">
+          <select
+            name="date"
+            className="border-none bg-inherit checked:text-blue-100"
+          >
+            <option value="today">2024.04.25</option>
+            <option value="tomorrow">2024.04.26</option>
+            <option value="twodays">2024.04.27</option>
+          </select>
+        </form>
+      </div>
+
       <div className="flex justify-between items-center">
         <div>
           <div className="font-semibold text-3xl">오늘의 날씨는</div>
@@ -16,10 +29,11 @@ export default function TodayWeather() {
         <div>
           <div className="flex items-center mb-3 border-2 border-white border-solid px-2 rounded-full">
             <FaArrowUp className="text-red-600" />
-            <span>28°C</span>
+            <span className="ml-2">28°C</span>
           </div>
           <div className="flex items-center border-2 border-white border-solid px-2 rounded-full">
-            <FaArrowDown className="text-blue-600" /> <span>10°C</span>
+            <FaArrowDown className="text-blue-600" />
+            <span className="ml-2">10°C</span>
           </div>
         </div>
       </div>
