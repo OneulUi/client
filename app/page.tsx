e6643c2e3ce8f4d9e386ec0afdfb2fad896cf006
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2024-05-04 16:10:43 +0900

    Fix:렌딩 페이지 컴포넌트가 말려올라가는 버그수정

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,14 @@ import bgImg from "../assets/bg.png";
 export default function Home() {
   return (
     <main className="w-full flex flex-col items-center justify-center">
-      <Image src={bgImg} width={500} height={500} alt="bgColor" />
-      <div className="flex flex-col items-center justify-center ">
+      <Image
+        src={bgImg}
+        width={500}
+        height={undefined}
+        className="h-[100vh]"
+        alt="bgColor"
+      />
+      <div className="flex flex-col items-center justify-center w-full">
         <Image
           src="https://cdn.pixabay.com/photo/2024/02/28/14/01/woman-8602128_1280.png"
           className="absolute top-24 w-[290px] h-[45vh] border-2 border-white rounded-2xl rotate-12"
@@ -23,14 +29,16 @@ export default function Home() {
           alt="girl"
           priority
         />
-        <div className="w-full absolute left-[200px] bottom-[200px] flex flex-col items-left">
-          <div className="font-extrabold text-[30px] mt-2">
-            <h1>오늘의 날씨에 맞는</h1>
-            <h1>옷차림을 보고 싶다면?</h1>
-          </div>
-          <div className="font-thin text-[15px] mt-2">
-            <h2>오늘의를 통해 온도별 옷차림을 확인하여</h2>
-            <h2>날씨에 맞는 옷을 입고 외출해 보세요!</h2>
+        <div className="relative w-full">
+          <div className="w-full absolute left-[30px] bottom-[200px] flex flex-col items-left">
+            <div className="font-extrabold text-[30px] mt-2">
+              <h1>오늘의 날씨에 맞는</h1>
+              <h1>옷차림을 보고 싶다면?</h1>
+            </div>
+            <div className="font-thin text-[15px] mt-2">
+              <h2>오늘의를 통해 온도별 옷차림을 확인하여</h2>
+              <h2>날씨에 맞는 옷을 입고 외출해 보세요!</h2>
+            </div>
           </div>
         </div>
 
