da51352d80a1072f5475ecd54cf530ea193aef49
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2024-05-04 15:56:19 +0900

    Destin:렌딩페이지 디자인 추가

--- api/api.ts
+++ api/api.ts
@@ -1,5 +1,25 @@
 import { Axios } from "./axios";
 
-export const getSurvey = async () => {
-  return await Axios.get("survey/options");
+export const getSurvey = async (params: GetSurveyParams) => {
+  const { baseDate, address } = params;
+  return await Axios.get("weather/current", {
+    params: { baseDate: baseDate, address: address },
+  });
+};
+
+export interface GetSurveyParams {
+  baseDate: string;
+  address: string;
+}
+
+export const getWeatherCurrent = async ({
+  params,
+}: {
+  params: GetSurveyParams;
+}) => {
+  //baseDate = params.baseData
+  const { baseDate, address } = params;
+  return await Axios.get("weather/current", {
+    params: { baseDate, address },
+  });
 };
--- app/page.tsx
+++ app/page.tsx
@@ -23,6 +23,17 @@ export default function Home() {
           alt="girl"
           priority
         />
+        <div className="w-full absolute left-[200px] bottom-[200px] flex flex-col items-left">
+          <div className="font-extrabold text-[30px] mt-2">
+            <h1>오늘의 날씨에 맞는</h1>
+            <h1>옷차림을 보고 싶다면?</h1>
+          </div>
+          <div className="font-thin text-[15px] mt-2">
+            <h2>오늘의를 통해 온도별 옷차림을 확인하여</h2>
+            <h2>날씨에 맞는 옷을 입고 외출해 보세요!</h2>
+          </div>
+        </div>
+
         <div className="absolute inset-x-0 bottom-20 flex justify-center">
           <Link href="/signin">
             <button className="bg-gray-900 text-white w-[45vh] h-[6vh] flex items-center justify-center rounded-3xl transition-transform hover:scale-95">
