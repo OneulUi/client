727b595bd9b99e56d077186b6b66fca2227d71eb
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2024-05-06 02:04:56 +0900

    Refactor:localstorage 함수화 후 suspense에 포함

--- app/login/redirect/page.tsx
+++ app/login/redirect/page.tsx
@@ -1,19 +1,26 @@
 "use client";
 import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
-import { Axios } from "@/api/axios";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-export default function Redirect() {
+
+function Search() {
+  const router = useRouter();
   const params = useSearchParams();
   const accessToken = params.get("accessToken");
   const loginId = JSON.stringify(params.get("loginId"));
-  const router = useRouter();
   useEffect(() => {
     localStorage.setItem("accessToken", `Bearer ${accessToken}`);
     localStorage.setItem("loginId", loginId);
     router.push("/survey");
   }, [router, accessToken, loginId]);
+  return <div>Loading...</div>;
+}
 
-  return <Suspense>Loading...</Suspense>;
+export default function Redirect() {
+  return (
+    <Suspense>
+      <Search />
+    </Suspense>
+  );
 }
