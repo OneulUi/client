1030146d943b20c8b8eeb1bc03b4a07040e0916b
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2024-05-18 13:33:04 +0900

    Design: Footer 아이콘 변경

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -4,14 +4,17 @@ import Link from "@/node_modules/next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 import image from "@/assets/sun.png";
-
+import { TiWeatherSunny } from "react-icons/ti";
+import { IoPersonOutline } from "react-icons/io5";
+import { BiAlignMiddle } from "react-icons/bi";
+import { RiHeart2Line } from "react-icons/ri";
 export default function Footer() {
   const pathName = usePathname();
   return (
     <footer className="fixed bottom-0 border-t-2 py-4 w-[500px] h-[10vh] text-sm bg-white">
       <div className="flex w-full items-center justify-around">
         <Link href="/main" className="flex flex-col items-center">
-          <Image width="20" height="20" src={image} alt="weather" />
+          <TiWeatherSunny size={27} className="text-gray-600" />
           {pathName === "/main" ? (
             <div className="text-blue-300">오늘 날씨</div>
           ) : (
@@ -19,7 +22,7 @@ export default function Footer() {
           )}
         </Link>
         <Link href="/recommend" className="flex flex-col items-center">
-          <Image width="20" height="20" src={image} alt="recommend" />
+          <BiAlignMiddle size={27} className="text-gray-600" />
           {pathName === "/recommend" ? (
             <div className="text-blue-300">날씨 옷 추천</div>
           ) : (
@@ -27,7 +30,7 @@ export default function Footer() {
           )}
         </Link>
         <Link href="/ootd" className="flex flex-col items-center">
-          <Image width="20" height="20" src={image} alt="ootd" />
+          <IoPersonOutline size={27} className="text-gray-600" />
           {pathName === "/ootd" ? (
             <div className="text-blue-300">날씨 OOTD</div>
           ) : (
@@ -35,7 +38,7 @@ export default function Footer() {
           )}
         </Link>
         <Link href="/mypage" className="flex flex-col items-center">
-          <Image width="20" height="20" src={image} alt="my" />
+          <RiHeart2Line size={27} className="text-gray-600" />
           {pathName === "/mypage" ? (
             <div className="text-blue-300">마이페이지</div>
           ) : (
